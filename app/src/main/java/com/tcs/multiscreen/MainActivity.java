75e1b2c1a26ca893d6bd3c9531e3ee3fd34f9eb2
Admin <admin@tcs-av7-04.local>
2020-12-31 15:21:45 +0530

    Initial commit

---
+++ app/src/main/java/com/tcs/multiscreen/MainActivity.java
@@ -0,0 +1,123 @@
+package com.tcs.multiscreen;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+
+public class MainActivity extends AppCompatActivity {
+
+    /****** Payment Success Result ******
+
+    {
+        "client": {
+        "environment": "sandbox",
+                "paypal_sdk_version": "2.15.3",
+                "platform": "Android",
+                "product_name": "PayPal-Android-SDK"
+    },
+        "response": {
+        "create_time": "2020-12-31T09:44:32Z",
+                "id": "PAYID-L7WZ3AA8HC13292M5547780A",
+                "intent": "sale",
+                "state": "approved"
+    },
+        "response_type": "payment"
+    }
+
+     ****** Payment Success Result End ******/
+
+
+    public static final int PAYPAL_REQUEST_CODE = 7171;
+
+    private static PayPalConfiguration configuration = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId("AcoaO0gEOI9MUavGOeMPC1kBajysZp5Ivr6RtW70lKMqWde_0v7z2IDKE6NfVWSSAEIHMtqzgmCA9QmI");
+
+    private Button btnPayNow;
+    private EditText edtAmount;
+
+    private String amount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /***** Start Starting Paypal Service *****/
+        Intent intent = new Intent(this, PayPalService.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,configuration);
+        startService(intent);
+        /***** End Starting Paypal Service *****/
+
+        edtAmount = findViewById(R.id.editAmout);
+        btnPayNow = findViewById(R.id.btnPay);
+
+        btnPayNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processPayment();
+            }
+        });
+    }
+
+    private void processPayment() {
+        amount = edtAmount.getText().toString();
+        PayPalPayment payPalPayment = new PayPalPayment(new BigDecimal(String.valueOf(amount)),
+                "USD", "DEMO TCS", PayPalPayment.PAYMENT_INTENT_SALE);
+        Intent intent = new Intent(this, PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,configuration);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
+        startActivityForResult(intent,PAYPAL_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirmation != null) {
+                    try {
+                        String paymentDetails = confirmation.toJSONObject().toString(4);
+                        Log.d("PAYMENT DETAILS => ", paymentDetails);
+
+                        JSONObject result = new JSONObject(paymentDetails);
+                        JSONObject response = result.getJSONObject("response");
+                        Log.d("Response =>", "PAY_ID:"+response.getString("id")+", "+ "PAY_STATUS:"+response.getString("state"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Toast.makeText(this, "Canceled", Toast.LENGTH_SHORT).show();
+        } else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+            Toast.makeText(this, "Invalid", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+}
